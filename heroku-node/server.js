59e57f46a0c37646f9589ab1fc5d0eb830dac8f5
Edna Liliana Galiano Camacho <alu0100762846@ull.edu.es>
2016-10-12 10:31:04 +0000

    Creado package.jason.

---
+++ heroku-node/server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var server = http.createSarver(function(req,res){
+    res.writeHead(200,{'Content-Type' : 
+    'text/html'});
+    res.end('<h1>Hello World<h1>');
+    
+});
+
+
+var port = Number(process.env.PORT || 3000 );
+
+
+server.listen(port);
