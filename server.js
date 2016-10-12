2cbe8f1497d0b10b7dc06c0f0f22ce02e20042f7
Edna Liliana Galiano Camacho <alu0100762846@ull.edu.es>
2016-10-12 10:07:03 +0000

    Creacion de server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createSarver(function(req,res){
+    res.writeHead(200,{'Content-Type' : 
+    'text/html'});
+    res.end('<h1> Hello World<h1>');
+    
+});
+
+server.listen(3000);
